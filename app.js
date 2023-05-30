b36a9c023f2ea901f68903ef0d94d8ec9f73ae18
melika0000 <mmelika113@gmail.com>
2023-05-30 21:06:47 +0330

    added primary html and css also images

---
+++ app.js
@@ -0,0 +1,8 @@
+function openFilter() {
+    document.getElementById("filter").style.visibility = "visible";
+    document.getElementById("filter").style.opacity = "1";
+  }
+function closeFilter() {
+    document.getElementById("filter").style.visibility = "hidden";
+    document.getElementById("filter").style.opacity = "0";
+}
